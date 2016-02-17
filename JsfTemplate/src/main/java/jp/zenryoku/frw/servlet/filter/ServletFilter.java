403c9961211c64602c9c2993fb4f9efb9267091d
ZenryokuService <tak4545@outlook.jp>
2016-02-17 23:08:25 +0900

    2016/02/17 Add PhaseListener and ServletFilter

--- JsfTemplate/src/main/java/jp/zenryoku/frw/servlet/filter/ServletFilter.java
+++ JsfTemplate/src/main/java/jp/zenryoku/frw/servlet/filter/ServletFilter.java
@@ -13,28 +13,28 @@ import javax.servlet.ServletResponse;
  * <ul>
  * <li>"/filters/フィルター名.xml"を読み込み</li>
  * <li>クラスをロードして、フィルター処理を行う、登録されたクラスをリストで保持、実行される</li>
- * 
+ * </ul>
  * @author ZenryokuService
  */
 public class ServletFilter implements Filter {
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.println("*** Filter.destroy *** ");
 		
 	}
 
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		
+		System.out.println("*** Filter.doFilter(upper) *** ");
+		chain.doFilter(req, res);
+		System.out.println("*** Filter.doFilter(lower) *** ");
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
+		System.out.println("*** Filter.init *** ");
 	}
 	
 }
